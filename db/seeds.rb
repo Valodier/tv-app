7847e5f64cab4d365eb970e1435ff9b060f5db90
William Meyer <valodier@icloud.com>
2022-04-19 08:37:34 -0400

    Seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Show.create! ([{title: "Friends", plot: "Friends do friend stuff in an apartment in NY, I think", year: "1965"}, {title:"The Stand", plot: "U.S. Gov makes a virus, like 1% of the pop lives", year: "0001"}, {title: "Rick and Morty", plot: "Seriously damaged man takes control of the universe to make sure he doesn't destroy it, purely for selfish reasons", year: "2100"}])
